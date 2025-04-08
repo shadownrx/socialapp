882a502ebd30d6e82902e4c94cbf8a8c789dcb39
salvador juarez <101913799+shadownrx@users.noreply.github.com>
2025-04-08 01:32:23 -0300

    not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+// app/not-found.tsx
+export default function NotFound() {
+    return (
+      <div className="text-center p-10">
+        <h1 className="text-2xl font-bold">404 - Página no encontrada</h1>
+        <p>Lo sentimos, la página que buscas no existe.</p>
+      </div>
+    );
+  }
+  
